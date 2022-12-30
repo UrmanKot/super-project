b3a842595b08da50553f26ef212646b7bf77c47c
v.trubicin <v.trubicin@unitcode.ru>
2022-12-30 12:56:38 +0300

    Auto-generated commit [ci skip]

--- main.js
+++ main.js
@@ -29353,7 +29353,7 @@ const environment = {
     sales_url: 'sales/',
     business_trips_url: 'business-trips/',
     image_path: 'https://rwave.pekauto.com',
-    link_url: 'https://pekauto.unitcode.ru',
+    link_url: 'https://pekauto.unitcode.ru/',
     // link_url: 'http://localhost:4201/',
     // link_url: 'https://rwave.pekauto.com/'
 };
