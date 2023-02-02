c23b739b86815de29e2bb979df6a9c7e5f32876c
SejimF <kolodkomaxim@gmail.com>
2023-02-02 15:05:37 +0300

    9780289 - отображать только контакты выбранных компаний при создании евента

--- src/app/crm/pickers/crm-multi-contact-persons-picker/crm-multi-contact-persons-picker.component.ts
+++ src/app/crm/pickers/crm-multi-contact-persons-picker/crm-multi-contact-persons-picker.component.ts
@@ -1,15 +1,18 @@
-import {Component, EventEmitter, Input, OnInit, Output, SimpleChanges} from '@angular/core';
+import {Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges} from '@angular/core';
 import {ContactPerson} from '@shared/models/contact-person';
 import {Subject, takeUntil} from 'rxjs';
 import {ContactPersonService} from '../../services/contact-person.service';
+import {QuerySearch} from '@shared/models/other';
 
 @Component({
   selector: 'pek-crm-multi-contact-persons-picker',
   templateUrl: './crm-multi-contact-persons-picker.component.html',
   styleUrls: ['./crm-multi-contact-persons-picker.component.scss']
 })
-export class CrmMultiContactPersonsPickerComponent implements OnInit {
+export class CrmMultiContactPersonsPickerComponent implements OnInit, OnChanges {
   @Input() currentContactPersonsIds: number[] = [];
+  @Input() isDisabled: boolean;
+  @Input() selectedCompaniesIds: number[];
   @Output() selectContactPersons: EventEmitter<number[]> = new EventEmitter<number[]>();
 
   isLoading = true;
@@ -24,19 +27,38 @@ export class CrmMultiContactPersonsPickerComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.contactPersonService.get().pipe(
-      takeUntil(this.destroy$)
-    ).subscribe(contactPersons => {
-      this.contactPersons = contactPersons;
-      this.findContactPersons();
-      this.isLoading = false;
-    });
+    this.getContact();
   }
 
   ngOnChanges(changes: SimpleChanges) {
+    if ('selectedCompaniesIds' in changes) {
+      this.getContact(true);
+    }
     if ('currentContactPersonsIds' in changes) {
       this.selectedContactPersons = [];
       this.findContactPersons();
+    }
+  }
+
+
+
+  getContact(excludeNonCompaniesContacts = false) {
+    let query: QuerySearch[];
+    if (this.selectedCompaniesIds) {
+      query = [{
+        name: 'company_ids',
+        value: this.selectedCompaniesIds
+      }];
+      this.contactPersonService.getForce(query).pipe(
+        takeUntil(this.destroy$)
+      ).subscribe(contactPersons => {
+        this.contactPersons = contactPersons;
+        this.findContactPersons();
+        if (excludeNonCompaniesContacts) {
+          this.updateRemainingSelectedPersons();
+        }
+        this.isLoading = false;
+      });
     }
   }
 
@@ -54,7 +76,10 @@ export class CrmMultiContactPersonsPickerComponent implements OnInit {
         const findContactPerson = this.contactPersons.find(t => t.id === id);
 
         if (findContactPerson) {
-          this.selectedContactPersons.push(findContactPerson);
+          const isExists = this.selectedContactPersons.some(person => person.id === findContactPerson.id);
+          if (!isExists) {
+            this.selectedContactPersons.push(findContactPerson);
+          }
         }
       });
     }
@@ -63,5 +88,15 @@ export class CrmMultiContactPersonsPickerComponent implements OnInit {
   ngOnDestroy() {
     this.destroy$.next(true);
     this.destroy$.complete();
+  }
+
+  private updateRemainingSelectedPersons() {
+    if (this.currentContactPersonsIds.length > 0 && this.selectedCompaniesIds.length > 0) {
+      const remainingPersons = this.currentContactPersonsIds.filter(contactId => this.contactPersons.findIndex(person => person.id === contactId) > -1);
+      this.selectContactPersons.emit(remainingPersons);
+    } else {
+      this.currentContactPersonsIds = [];
+      this.selectContactPersons.emit(null);
+    }
   }
 }
