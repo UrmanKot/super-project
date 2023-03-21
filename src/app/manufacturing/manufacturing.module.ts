ed3183a8c698e74c513be6b41ae2c5a6c9730d5b
v.trubicin <v.trubicin@unitcode.ru>
2023-03-21 16:51:52 +0300

    add module

---
+++ src/app/manufacturing/manufacturing.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ManufacturingRouting } from './manufacturing.routing';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ManufacturingRouting
+  ]
+})
+export class ManufacturingModule { }
